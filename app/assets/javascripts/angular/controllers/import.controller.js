efb945f85bb37c76003aa183c53ff29697049df2
Mike Crockett <you@somedomain.com>
2014-11-10 17:34:23 -0600

    Adding ability to add game id.

--- app/assets/javascripts/angular/controllers/import.controller.js
+++ app/assets/javascripts/angular/controllers/import.controller.js
@@ -3,10 +3,12 @@ app.controller('ImportController', ['$scope', 'Leagues', 'FanDuelData', function
   $scope.selectedLeague = "NONE";
   $scope.fan_duel_data = "";
   $scope.message = "";
+  $scope.fan_duel_game_id = null;
   $scope.add_fan_duel_json = function() {
     new FanDuelData({
       data:$scope.fan_duel_data,
-      league:$scope.selectedLeague
+      league:$scope.selectedLeague,
+      fd_game_id:$scope.fan_duel_game_id
     }).$save({},
       function(v){
         $scope.fan_duel_data="";
