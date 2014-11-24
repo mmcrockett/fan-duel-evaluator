39e28cc86aec1e6a7522a731365ad3189ff50269
Mike Crockett <you@somedomain.com>
2014-11-24 16:14:22 -0600

    Testing analyzing.

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -6,16 +6,28 @@ class Roster < ActiveRecord::Base
   attr_accessor :players
 
   #COLUMNS = [:avg, :max, :min, :mean, :med]
-  COLUMNS = [:max, :min, :med, :expp]
+  #COLUMNS = [:max, :min, :med, :expp]
+  COLUMNS = [:max]
 
   def self.analyze(league)
     puts "Start Time: #{Time.now}"
     start_time = Time.now
-    strategies = [:xheavy, :heavy, :balanced]
-
     players    = FanDuelPlayer.player_data({:league => league})
-    positions  = players.first.class::POSITIONS
     import_id  = players.first.import_id
+    positions  = players.first.class::POSITIONS
+    budget     = players.first.class::BUDGET
+
+    best_rosters = Roster.get_best_rosters(players, positions, budget)
+
+    best_rosters.each_pair do |col, roster|
+      Roster.create({:import_id => import_id, :notes => "#{col}", :player_ids => roster.player_ids})
+    end
+
+    puts "Exec Time: #{Time.now - start_time}"
+  end
+
+  def self.get_best_rosters(players, positions, budget)
+    strategies = [:xheavy, :heavy, :valuebalanced, :bestbalanced]
     position_permutations = positions.permutation(positions.size).to_a
 
     best_rosters = {}
@@ -24,29 +36,33 @@ class Roster < ActiveRecord::Base
       best_rosters[sort_column] = nil
       sorted_players = FanDuelPlayer.sort(players, sort_column)
       player_finder  = PlayerFinder.new(sorted_players)
-      post_optimize  = true
+      #post_optimize  = true
+      post_optimize  = false
 
       position_permutations.each do |position_order|
         rosters = {}
         strategies.each do |strategy|
-          key = "#{strategy}"
-          [:standard, :variable].each do |substrategy|
-            begin
-              key += "_#{substrategy}"
-              rosters[key] = SimpleRoster.new(players[0].class::BUDGET, positions.size, sort_column)
+          key   = "#{strategy}"
+          force = false
+          rosters[key] = SimpleRoster.new(budget, positions.size, sort_column)
+          i = 0
 
-              position_order.each_with_index do |position, i|
-                if (true == Roster.find_best?(strategy, i))
-                  rosters[key] << player_finder.find_best(position, {:exclude => rosters[key].players})
-                elsif ((i < (positions.size - 3)) && (:variable == substrategy))
-                  rosters[key] << player_finder.find_value(position, {:max_cost => (rosters[key].remaining_avg_budget*1.2).to_i, :exclude => rosters[key].players})
-                elsif (i < (positions.size - 1))
-                  rosters[key] << player_finder.find_value(position, {:max_cost => rosters[key].remaining_avg_budget, :exclude => rosters[key].players})
-                else
-                  rosters[key] << player_finder.find_best(position, {:max_cost => rosters[key].remaining_budget, :exclude => rosters[key].players})
-                end
+          while (i < position_order.size)
+            position = position_order[i]
+            begin
+              if (true == Roster.find_best?(strategy, i))
+                rosters[key] << player_finder.find_best(position, {:exclude => rosters[key].players})
+              elsif (true == Roster.find_best_capped?(strategy, i, positions.size))
+                rosters[key] << player_finder.find_best(position, {:max_cost => (rosters[key].remaining_avg_budget*1.2).to_i, :exclude => rosters[key].players})
+              elsif (i < (positions.size - 1))
+                rosters[key] << player_finder.find_value(position, {:max_cost => rosters[key].remaining_avg_budget*1.2, :exclude => rosters[key].players})
+              else
+                rosters[key] << player_finder.find_best(position, {:max_cost => rosters[key].remaining_budget, :exclude => rosters[key].players})
               end
-            rescue PlayerFinderValidPlayerNotFoundException
+              i += 1
+            rescue PlayerFinderValidPlayerNotFoundException,SimpleRosterBudgetException
+              rosters[key].delete(rosters[key].players[-1])
+              i -= 1
             end
           end
         end
@@ -84,17 +100,21 @@ class Roster < ActiveRecord::Base
       end
     end
 
-    best_rosters.each_pair do |col, roster|
-      Roster.create({:import_id => import_id, :notes => "#{col}", :player_ids => roster.player_ids})
-    end
-
-    puts "Exec Time: #{Time.now - start_time}"
+    return best_rosters
   end
 
   def self.find_best?(strategy, i)
     if ((:xheavy == strategy) && (i < 3))
       return true
     elsif ((:heavy == strategy) && (i < 2))
+      return true
+    end
+
+    return false
+  end
+
+  def self.find_best_capped?(strategy, i, length)
+    if ((:rbalanced == strategy) && (i < (length - 1)))
       return true
     end
 
--- test/models/roster_test.rb
+++ test/models/roster_test.rb
@@ -1,7 +1,66 @@
 require 'test_helper'
 
+class TestPlayer < FanDuelPlayer
+  POSITIONS  = ["X","X"]
+  BUDGET     = 100
+end
+
 class RosterTest < ActiveSupport::TestCase
-  test "the truth" do
-  #   assert true
+  def setup
+    @players = []
+    @players << TestPlayer.new({
+      :id        => 0,
+      :position  => "X",
+      :cost      => 5,
+      :game_data => [0,10,-2]
+    })
+    @players << TestPlayer.new({
+      :id        => 1,
+      :position  => "X",
+      :cost      => 5,
+      :game_data => [2,1,9]
+    })
+    @players << TestPlayer.new({
+      :id        => 2,
+      :position  => "X",
+      :cost      => 5,
+      :game_data => [6,4,0]
+    })
+    @players << TestPlayer.new({
+      :id        => 3,
+      :position  => "X",
+      :cost      => 5,
+      :game_data => [4,4,4]
+    })
+  end
+
+  test "best rosters" do
+    @players[0].cost = 70
+    @players[1].cost = 50
+    @players[2].cost = 50
+    @players[3].cost = 20
+
+    best_rosters = Roster.get_best_rosters(@players, TestPlayer::POSITIONS, TestPlayer::BUDGET)
+
+    max_roster = best_rosters[:max]
+    #med_roster = best_rosters[:med]
+    #min_roster = best_rosters[:min]
+
+    assert_equal([1,2], max_roster.player_ids)
+    #assert_equal([2,3], med_roster.player_ids)
+    #assert_equal([1,3], min_roster.player_ids)
+  end
+
+  test "absolute best rosters" do
+    return
+    best_rosters = Roster.get_best_rosters(@players, TestPlayer::POSITIONS, TestPlayer::BUDGET)
+
+    max_roster = best_rosters[:max]
+    med_roster = best_rosters[:med]
+    min_roster = best_rosters[:min]
+
+    assert_equal([0,1], max_roster.player_ids)
+    assert_equal([2,3], med_roster.player_ids)
+    assert_equal([1,3], min_roster.player_ids)
   end
 end
