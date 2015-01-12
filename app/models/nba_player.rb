99c5c6436b4ecbff342859d789797d10f82abfc8
Mike Crockett <you@somedomain.com>
2015-01-12 16:37:30 -0600

    Adding nba-stats parsing.

--- app/models/nba_player.rb
+++ app/models/nba_player.rb
@@ -1,9 +1,46 @@
+require 'uri'
+require 'open-uri'
+
 class NbaPlayer < FanDuelPlayer
   MAX_GAMES = 10
   MAX_DATES = (MAX_GAMES * 2.2).to_i
 
   POSITIONS  = ["PG","PG","SG","SG","SF","SF","PF","PF","C"]
   BUDGET     = 60000
+
+  NBA_STATS_URLS = {
+    'team' => "http://stats.nba.com/stats/leaguedashteamstats",
+    'roster' => "http://stats.nba.com/stats/teamplayerdashboard",
+    'player' => "http://stats.nba.com/stats/playergamelog",
+  }
+  NBA_STATS_PARAMS = {
+    "DateFrom"         => nil,
+    "DateTo"           => nil,
+    "GameScope"        => nil,
+    "GameSegment"      => nil,
+    "LastNGames"       => 0,
+    "LeagueID"         => "00",
+    "Location"         => nil,
+    "MeasureType"      => "Advanced",
+    "Month"            => 0,
+    "OpponentTeamID"   => 0,
+    "Outcome"          => nil,
+    "PaceAdjust"       => "N",
+    "PerMode"          => "Totals",
+    "Period"           => 0,
+    "PlayerExperience" => nil,
+    "PlayerPosition"   => nil,
+    "PlayerID"         => 0,
+    "PlusMinus"        => "N",
+    "Rank"             => "N",
+    "Season"           => "2014-15",
+    "SeasonSegment"    => nil,
+    "SeasonType"       => "Regular Season",
+    "StarterBench"     => nil,
+    "TeamID"           => 0,
+    "VsConference"     => nil,
+    "VsDivision"       => nil,
+  }
 
   TEAMS_BY_FD_ID = {
     679 => "ATL",
@@ -40,5 +77,13 @@ class NbaPlayer < FanDuelPlayer
 
   def important?
     return (8 < self.average)
+  end
+
+  def self.parse_team_data
+    uri = URI("#{NBA_STATS_URLS['player']}")
+    puts "#{uri}"
+    uri.query = NBA_STATS_PARAMS.merge({"PlayerID" => 200765}).to_query
+    puts "#{uri}"
+    return JSON.parse(open(uri).read())
   end
 end
