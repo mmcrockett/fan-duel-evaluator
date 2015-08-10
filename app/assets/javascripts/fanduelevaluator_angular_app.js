4b296963f79319b2dfe94c169d741c2051c15a65
Mike Crockett <mcrockett>
2015-08-10 16:10:55 -0500

    Moving to local storage instead of cookie storage.

--- app/assets/javascripts/fanduelevaluator_angular_app.js
+++ app/assets/javascripts/fanduelevaluator_angular_app.js
@@ -1,4 +1,7 @@
-var app = angular.module('FanDuelEvaluator', ['ngResource', 'ngCookies', 'googlechart', 'ui.bootstrap', 'angular.filter']);
+var app = angular.module('FanDuelEvaluator', ['ngResource', 'googlechart', 'ui.bootstrap', 'angular.filter', 'LocalStorageModule']);
 app.config(["$httpProvider", function(provider) {
   provider.defaults.headers.common['X-CSRF-Token'] = jQuery('meta[name=csrf-token]').attr('content');
 }]);
+app.config(function(localStorageServiceProvider) {
+  localStorageServiceProvider.setPrefix('fantasy_evaluator');
+});
